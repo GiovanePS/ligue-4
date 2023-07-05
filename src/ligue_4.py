671728721deccff278e13fd945ec5c019a6bcb6e
Giovane Pimentel de Sousa <giovanespace@gmail.com>
2023-07-05 18:05:13 -0300

    commit 2

--- src/ligue_4.py
+++ src/ligue_4.py
@@ -1,22 +1,11 @@
-
 class Tabuleiro:
-    def __init__(self, largura, altura):
-        self.__largura = largura
-        self.__altura = altura
+    def __init__(self):
         self.__tabuleiro = []
         self.montar_tabuleiro()
 
     def montar_tabuleiro(self):
-        self.__tabuleiro = [[0 for _ in range(self.largura)] for __ in range(self.altura)]
+        self.__tabuleiro = [[0 for _ in range(7)] for __ in range(6)]
 
-    @property
-    def largura(self):
-        return self.__largura
-
-    @property
-    def altura(self):
-        return self.__altura
-    
     @property
     def tabuleiro(self):
         return self.__tabuleiro
@@ -37,20 +26,28 @@ class Tabuleiro:
                 
             print("Coluna cheia. Jogue em outro coluna!")
 
-    def verificar_vitoria
+    def verificar_vitoria():
+        ...
 
-def iniciar_jogo():
-    largura = 7
-    altura = 6
-    tabuleiro = Tabuleiro(largura, altura)
-    jogador = 0
-    while True:
-        tabuleiro.mostrar_tabuleiro()
-        tabuleiro.adicionar_peca(jogador%2+1)
-        jogador += 1
-        if jogador == largura * altura - 1:
-            print("Empate")
-            return
+class Ligue4:
+    def __init__(self):
+        self.tabuleiro = Tabuleiro()
+
+    def iniciar_jogo(self):
+        jogador = 0
+        while True:
+            self.tabuleiro.mostrar_tabuleiro()
+            self.tabuleiro.adicionar_peca(jogador%2+1)
+            jogador += 1
+            if jogador == 6 * 7 - 1:
+                print("Empate")
+                return
+            
+    def verificar_empate(self):
+        ...
+
+    def verificar_vitoria(self):
+        ...
 
 if __name__ == "__main__":
-    iniciar_jogo()
+    Ligue4.iniciar_jogo()
